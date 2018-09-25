ac9ded8cc605f31b46524493876ed74df6ab0375
Enzoj0se <enzo190100@gmail.com>
2018-09-24 21:45:41 -0300

    acrescimo de dois campos na tela de cadasro  de disciplina

--- src/views/CadastroDisciplinaWindow.java
+++ src/views/CadastroDisciplinaWindow.java
@@ -21,8 +21,12 @@ public class CadastroDisciplinaWindow extends JInternalFrame{
 	private JLabel lblCreditos;
 	private JLabel lblCurso;
 	private JLabel lblDia;
+	private JLabel lblCodigo;
+	private JLabel lblNumProfessores;
 	private JTextField txfNome;
 	private JFormattedTextField ftxfCreditos;
+	private JFormattedTextField ftxfCodigo;
+	private JFormattedTextField ftxfNumProfessores;
 	private JTextField txtCurso;
 	private JComboBox comboDia;
 	private JComboBox comboFase;
@@ -38,7 +42,7 @@ public class CadastroDisciplinaWindow extends JInternalFrame{
 	
 	
 	public CadastroDisciplinaWindow() {
-		setSize(500, 200);
+		setSize(500, 230);
 		setTitle("Cadastro de Disciplina");
 		setLayout(null);
 		
@@ -57,57 +61,80 @@ public class CadastroDisciplinaWindow extends JInternalFrame{
 	
 	private void criarComponentes() {
 //		=====================Label's========================
+		
+		lblCodigo = new JLabel("Código:");
+		lblCodigo.setBounds(15, 10, 50, 25);
+		getContentPane().add(lblCodigo);
+		
+		lblNumProfessores = new JLabel("Nº Prof:");
+		lblNumProfessores.setBounds(200, 10, 50, 25);
+		getContentPane().add(lblNumProfessores);
+		
 		lblNome = new JLabel("Nome:");
-		lblNome.setBounds(15, 10, 50, 25);
+		lblNome.setBounds(15, 40, 50, 25);
 		getContentPane().add(lblNome);
 		
 		lblFase = new JLabel("Fase:");
-		lblFase.setBounds(15, 40, 50, 25);
+		lblFase.setBounds(15, 70, 50, 25);
 		getContentPane().add(lblFase);
 		
 		lblCreditos = new JLabel("Créditos:");
-		lblCreditos.setBounds(15, 70, 60, 25);
+		lblCreditos.setBounds(15, 100, 60, 25);
 		getContentPane().add(lblCreditos);
 		
 		lblCurso = new JLabel("Curso:");
-		lblCurso.setBounds(200, 40, 300, 25);
+		lblCurso.setBounds(200, 70, 300, 25);
 		getContentPane().add(lblCurso);
 		
 		lblDia = new JLabel("Dia:");
-		lblDia.setBounds(200, 70, 300, 25);
+		lblDia.setBounds(200, 100, 300, 25);
 		getContentPane().add(lblDia);
 //		====================textFields / FormattedTextFields========================
 		txfNome = new JTextField();
-		txfNome.setBounds(80, 10, 350, 25);
+		txfNome.setBounds(80, 40, 350, 25);
 		getContentPane().add(txfNome);
 		
 		txtCurso = new JTextField();
-		txtCurso.setBounds(250, 40, 180, 25);
+		txtCurso.setBounds(250, 70, 180, 25);
 		getContentPane().add(txtCurso);
 		
 		try {
 			ftxfCreditos = new JFormattedTextField(maskFase("##"));
-			ftxfCreditos.setBounds(80, 70, 100, 25);
+			ftxfCreditos.setBounds(80, 100, 100, 25);
 			getContentPane().add(ftxfCreditos);
 		} catch (ParseException e1) {
 			e1.printStackTrace();
 		}
 		
+		try {
+			ftxfCodigo = new JFormattedTextField(maskFase("#####"));
+			ftxfCodigo.setBounds(80, 10, 100, 25);
+			getContentPane().add(ftxfCodigo);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
 		
+		try {
+			ftxfNumProfessores = new JFormattedTextField(maskFase("#####"));
+			ftxfNumProfessores.setBounds(250, 10, 180, 25);
+			getContentPane().add(ftxfNumProfessores);
+		} catch (ParseException e1) {
+			e1.printStackTrace();
+		}
 //		====================ComboBox===============================
 		comboDia = new JComboBox(diaSemana);
 		comboDia.setSelectedIndex(0);
-		comboDia.setBounds(250, 70, 180, 25);
+		comboDia.setBounds(250, 100, 180, 25);
 		getContentPane().add(comboDia);
 		
 		comboFase = new JComboBox(fases);
 		comboFase.setSelectedIndex(0);
-		comboFase.setBounds(80, 40, 100, 25);
+		comboFase.setBounds(80, 70, 100, 25);
 		getContentPane().add(comboFase);
 		
 //		=====================Buttons================================
 		btnConcluir = new JButton("Concluir");
-		btnConcluir.setBounds(100, 110, 250, 30);
+		btnConcluir.setBounds(100, 140, 250, 30);
 		getContentPane().add(btnConcluir);
 		btnConcluir.addActionListener(new ActionListener() {
 
