85054961ea655f2558f610015fc30d5451bd635a
SkaZZ9 <eduardoamerico_@live.com>
2018-10-17 19:40:46 -0300

    Modificação no teste

--- src/views/Teste.java
+++ src/views/Teste.java
@@ -7,60 +7,77 @@ public class Teste {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Map<String, String> mapaNomes = new HashMap<String, String>(); 
-	    mapaNomes.put("10850", "Algoritmos");
-	    mapaNomes.put("10851", "Introdução a Ciência da Computação");
-	    mapaNomes.put("10854", "Fundamentos Matemáticos para Computação");
-	    mapaNomes.put("10852", "Metodologia Científica e da Pesquisa");
-	    mapaNomes.put("10853", "Produção e Interpretação de Textos");
-	    mapaNomes.put("10858", "Cálculo I");
-	    mapaNomes.put("10857", "Filosofia");
-	    mapaNomes.put("10856", "Física Aplicada");
-	    mapaNomes.put("10855", "Lógica para Computação");
-	    mapaNomes.put("10859", "Programação I");
-	    mapaNomes.put("10860", "Cálculo II");
-	    mapaNomes.put("10862", "Circuitos Digitais");
-	    mapaNomes.put("10863", "Estatística e Probabilidade");
-	    mapaNomes.put("10864", "Estrutura de Dados I");
-	    mapaNomes.put("10861", "Programação II");
-	    mapaNomes.put("10865", "Projeto Interdisciplinar da Computação I");
-	    mapaNomes.put("10868", "Análise de Algoritmos");
-	    mapaNomes.put("10869", "Arquitetura e Organização de Computadores");
-	    mapaNomes.put("10870", "Cálculo Numérico");
-	    mapaNomes.put("10866", "Estrutura de Dados II");
-	    mapaNomes.put("10867", "Projeto Interdisciplinar da Computação II");
-	    mapaNomes.put("10871", "Teoria dos Grafos");
-	    mapaNomes.put("10875", "Banco de Dados I");
-	    mapaNomes.put("10876", "Engenharia de Software I");
-	    mapaNomes.put("10873", "Linguagens Formais");
-	    mapaNomes.put("10872", "Sistemas Operacionais");
-	    mapaNomes.put("10874", "Tópicos Especiais I");
-	    mapaNomes.put("10877", "Banco de Dados II");
-	    mapaNomes.put("10879", "Compiladores");
-	    mapaNomes.put("10880", "Comunicação de Dados");
-	    mapaNomes.put("10878", "Engenharia de Software II");
-	    mapaNomes.put("10881", "Teoria da Computação");
-	    mapaNomes.put("10886", "Administração Empresarial");
-	    mapaNomes.put("10884", "Computação Gráfica");
-	    mapaNomes.put("10883", "Computação Paralela e Distribuída");
-	    mapaNomes.put("10885", "Inteligência Artificial");
-	    mapaNomes.put("10882", "Redes de Computadores");
-	    mapaNomes.put("10887", "Trabalho de Conclusão de Curso - TCC I");
-	    mapaNomes.put("10891", "Empreendedorismo");
-	    mapaNomes.put("10890", "Gestão de Sistemas de Informação");
-	    mapaNomes.put("10892", "Optativa I");
-	    mapaNomes.put("10888", "Tópicos Especiais II");
-	    mapaNomes.put("10889", "Trabalho de Conclusão de Curso - TCC II");
-	    mapaNomes.put("10895", "Optativa II");
-	    mapaNomes.put("10893", "Tópicos Especiais III");
-	    mapaNomes.put("10894", "Trabalho de Conclusão de Curso - TCC III");
+		Map<String, String> mapDisciplinas = new HashMap<String, String>(); 
+	    mapDisciplinas.put("10850", "Algoritmos");
+	    mapDisciplinas.put("10851", "Introdução a Ciência da Computação");
+	    mapDisciplinas.put("10854", "Fundamentos Matemáticos para Computação");
+	    mapDisciplinas.put("10852", "Metodologia Científica e da Pesquisa");
+	    mapDisciplinas.put("10853", "Produção e Interpretação de Textos");
+	    mapDisciplinas.put("10858", "Cálculo I");
+	    mapDisciplinas.put("10857", "Filosofia");
+	    mapDisciplinas.put("10856", "Física Aplicada");
+	    mapDisciplinas.put("10855", "Lógica para Computação");
+	    mapDisciplinas.put("10859", "Programação I");
+	    mapDisciplinas.put("10860", "Cálculo II");
+	    mapDisciplinas.put("10862", "Circuitos Digitais");
+	    mapDisciplinas.put("10863", "Estatística e Probabilidade");
+	    mapDisciplinas.put("10864", "Estrutura de Dados I");
+	    mapDisciplinas.put("10861", "Programação II");
+	    mapDisciplinas.put("10865", "Projeto Interdisciplinar da Computação I");
+	    mapDisciplinas.put("10868", "Análise de Algoritmos");
+	    mapDisciplinas.put("10869", "Arquitetura e Organização de Computadores");
+	    mapDisciplinas.put("10870", "Cálculo Numérico");
+	    mapDisciplinas.put("10866", "Estrutura de Dados II");
+	    mapDisciplinas.put("10867", "Projeto Interdisciplinar da Computação II");
+	    mapDisciplinas.put("10871", "Teoria dos Grafos");
+	    mapDisciplinas.put("10875", "Banco de Dados I");
+	    mapDisciplinas.put("10876", "Engenharia de Software I");
+	    mapDisciplinas.put("10873", "Linguagens Formais");
+	    mapDisciplinas.put("10872", "Sistemas Operacionais");
+	    mapDisciplinas.put("10874", "Tópicos Especiais I");
+	    mapDisciplinas.put("10877", "Banco de Dados II");
+	    mapDisciplinas.put("10879", "Compiladores");
+	    mapDisciplinas.put("10880", "Comunicação de Dados");
+	    mapDisciplinas.put("10878", "Engenharia de Software II");
+	    mapDisciplinas.put("10881", "Teoria da Computação");
+	    mapDisciplinas.put("10886", "Administração Empresarial");
+	    mapDisciplinas.put("10884", "Computação Gráfica");
+	    mapDisciplinas.put("10883", "Computação Paralela e Distribuída");
+	    mapDisciplinas.put("10885", "Inteligência Artificial");
+	    mapDisciplinas.put("10882", "Redes de Computadores");
+	    mapDisciplinas.put("10887", "Trabalho de Conclusão de Curso - TCC I");
+	    mapDisciplinas.put("10891", "Empreendedorismo");
+	    mapDisciplinas.put("10890", "Gestão de Sistemas de Informação");
+	    mapDisciplinas.put("10892", "Optativa I");
+	    mapDisciplinas.put("10888", "Tópicos Especiais II");
+	    mapDisciplinas.put("10889", "Trabalho de Conclusão de Curso - TCC II");
+	    mapDisciplinas.put("10895", "Optativa II");
+	    mapDisciplinas.put("10893", "Tópicos Especiais III");
+	    mapDisciplinas.put("10894", "Trabalho de Conclusão de Curso - TCC III");
 	   
+	    Map<String, String> mapDiasSemana = new HashMap<String, String>(); 
+	    mapDiasSemana.put("01", "Domingo");
+	    mapDiasSemana.put("02", "Segunda-Feira");
+	    mapDiasSemana.put("03", "Terça-Feira");
+	    mapDiasSemana.put("04", "Quarta-Feira");
+	    mapDiasSemana.put("05", "Quinta-Feira");
+	    mapDiasSemana.put("06", "Sexta-Feira");
+	    mapDiasSemana.put("07", "Sabado");
 	    
-	 
-	//System.out.println(mapaNomes);
+	    Map<String, String> mapQualificao = new HashMap<String, String>(); 
+	    mapQualificao.put("01", "Pós-Graduação");
+	    mapQualificao.put("02", "Mestrado");
+	    mapQualificao.put("03", "Doutorado");
+	    mapQualificao.put("04", "Pós-Doutorado");
+
+	    
+	    
+	//System.out.println(mapDisciplinas);
 	     
 	    //resgatando o nome da posição 2
-	    System.out.println(mapaNomes.get("10894"));
+	    System.out.println(mapDisciplinas.get("10873"));
+	    System.out.println(mapDiasSemana.get("03"));
+	    System.out.println(mapQualificao.get("04"));
 	}
 
 }
