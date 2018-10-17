5e8a4f3183284f5a6dc9af9b2cd6930ec4090a4f
SkaZZ9 <eduardoamerico_@live.com>
2018-10-17 19:15:13 -0300

    Criação classe teste para o hash map

---
+++ src/views/Teste.java
@@ -0,0 +1,66 @@
+package views;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<String, String> mapaNomes = new HashMap<String, String>(); 
+	    mapaNomes.put("10850", "Algoritmos");
+	    mapaNomes.put("10851", "Introdução a Ciência da Computação");
+	    mapaNomes.put("10854", "Fundamentos Matemáticos para Computação");
+	    mapaNomes.put("10852", "Metodologia Científica e da Pesquisa");
+	    mapaNomes.put("10853", "Produção e Interpretação de Textos");
+	    mapaNomes.put("10858", "Cálculo I");
+	    mapaNomes.put("10857", "Filosofia");
+	    mapaNomes.put("10856", "Física Aplicada");
+	    mapaNomes.put("10855", "Lógica para Computação");
+	    mapaNomes.put("10859", "Programação I");
+	    mapaNomes.put("10860", "Cálculo II");
+	    mapaNomes.put("10862", "Circuitos Digitais");
+	    mapaNomes.put("10863", "Estatística e Probabilidade");
+	    mapaNomes.put("10864", "Estrutura de Dados I");
+	    mapaNomes.put("10861", "Programação II");
+	    mapaNomes.put("10865", "Projeto Interdisciplinar da Computação I");
+	    mapaNomes.put("10868", "Análise de Algoritmos");
+	    mapaNomes.put("10869", "Arquitetura e Organização de Computadores");
+	    mapaNomes.put("10870", "Cálculo Numérico");
+	    mapaNomes.put("10866", "Estrutura de Dados II");
+	    mapaNomes.put("10867", "Projeto Interdisciplinar da Computação II");
+	    mapaNomes.put("10871", "Teoria dos Grafos");
+	    mapaNomes.put("10875", "Banco de Dados I");
+	    mapaNomes.put("10876", "Engenharia de Software I");
+	    mapaNomes.put("10873", "Linguagens Formais");
+	    mapaNomes.put("10872", "Sistemas Operacionais");
+	    mapaNomes.put("10874", "Tópicos Especiais I");
+	    mapaNomes.put("10877", "Banco de Dados II");
+	    mapaNomes.put("10879", "Compiladores");
+	    mapaNomes.put("10880", "Comunicação de Dados");
+	    mapaNomes.put("10878", "Engenharia de Software II");
+	    mapaNomes.put("10881", "Teoria da Computação");
+	    mapaNomes.put("10886", "Administração Empresarial");
+	    mapaNomes.put("10884", "Computação Gráfica");
+	    mapaNomes.put("10883", "Computação Paralela e Distribuída");
+	    mapaNomes.put("10885", "Inteligência Artificial");
+	    mapaNomes.put("10882", "Redes de Computadores");
+	    mapaNomes.put("10887", "Trabalho de Conclusão de Curso - TCC I");
+	    mapaNomes.put("10891", "Empreendedorismo");
+	    mapaNomes.put("10890", "Gestão de Sistemas de Informação");
+	    mapaNomes.put("10892", "Optativa I");
+	    mapaNomes.put("10888", "Tópicos Especiais II");
+	    mapaNomes.put("10889", "Trabalho de Conclusão de Curso - TCC II");
+	    mapaNomes.put("10895", "Optativa II");
+	    mapaNomes.put("10893", "Tópicos Especiais III");
+	    mapaNomes.put("10894", "Trabalho de Conclusão de Curso - TCC III");
+	   
+	    
+	 
+	//System.out.println(mapaNomes);
+	     
+	    //resgatando o nome da posição 2
+	    System.out.println(mapaNomes.get("10894"));
+	}
+
+}
